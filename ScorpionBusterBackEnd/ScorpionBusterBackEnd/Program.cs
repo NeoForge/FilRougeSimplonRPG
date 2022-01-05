bfbb9c1031c6bcd8ea450865fe40c9295372c815
ARMAND Constantin <constantin.armand@gmail.com>
2022-01-05 14:09:49 +0100

    BackEnd API Fonctionnel

--- ScorpionBusterBackEnd/ScorpionBusterBackEnd/Program.cs
+++ ScorpionBusterBackEnd/ScorpionBusterBackEnd/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ScorpionBusterBackEnd.Data.Scorpionbuster2Context>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ScorpionBuster2"));
+    }
+    );
 
 var app = builder.Build();
 
