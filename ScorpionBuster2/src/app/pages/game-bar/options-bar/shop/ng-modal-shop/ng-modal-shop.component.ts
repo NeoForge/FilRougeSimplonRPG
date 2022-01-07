0c87263ffc20c6a6353a28e784ed3ce7f3712a71
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2022-01-07 16:37:37 +0100

    add buy items

--- ScorpionBuster2/src/app/pages/game-bar/options-bar/shop/ng-modal-shop/ng-modal-shop.component.ts
+++ ScorpionBuster2/src/app/pages/game-bar/options-bar/shop/ng-modal-shop/ng-modal-shop.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { HeroService } from 'src/app/apiServices/hero.service';
 import { ItemsService } from 'src/app/apiServices/items.service';
+import { Hero } from 'src/app/models/hero';
+import { Item } from 'src/app/models/items';
 
 @Component({
   selector: 'app-ng-modal-shop',
@@ -10,10 +13,10 @@ import { ItemsService } from 'src/app/apiServices/items.service';
 export class NgModalShopComponent implements OnInit {
     itemShop: any;
     description: string = "";
-  constructor(public activeModal: NgbActiveModal,private ItemsService:ItemsService) { }
+
+  constructor(public activeModal: NgbActiveModal,private ItemsService:ItemsService, private HeroService: HeroService) { }
 
   ngOnInit(): void {
-    console.log("yo");
     this.ItemsService.GetItem().subscribe(
             (data: any) => {
               this.itemShop = data;
@@ -21,7 +24,18 @@ export class NgModalShopComponent implements OnInit {
       }
     );
   }
-  displayDescription(description: string){
+  choosenItem: number = 0;
+  displayDescription(description: string, id: number){
     this.description = description;
+    this.choosenItem = id;
+  }
+  onBuy(){
+    this.itemShop[this.choosenItem].ownedQuantity = this.itemShop[this.choosenItem].ownedQuantity + 1;
+    this.itemShop[this.choosenItem].owned = true;
+    this.ItemsService.PutItem(this.itemShop[this.choosenItem]).subscribe(
+            (data: any) => {
+              console.log(data);
+      }
+    );
   }
 }
