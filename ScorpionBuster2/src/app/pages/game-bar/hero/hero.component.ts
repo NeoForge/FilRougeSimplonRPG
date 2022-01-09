6ec2a95ef5c16e0802f91c8177ab11a77ffdecde
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2022-01-09 16:23:06 +0100

    add change hero image

--- ScorpionBuster2/src/app/pages/game-bar/hero/hero.component.ts
+++ ScorpionBuster2/src/app/pages/game-bar/hero/hero.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HeroService } from 'src/app/apiServices/hero.service';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
-
-  constructor() { }
+  heroId: number = 0;
+  hero: any;
+  constructor(private HeroService: HeroService) { }
 
   ngOnInit(): void {
+    if (localStorage.getItem("hero") === "Marty") {
+      this.heroId = 5;
+    } else if (localStorage.getItem("hero") === "Bill") {
+      this.heroId = 6;
+    }
+    this.HeroService.GetHeroById(this.heroId).subscribe(
+      (data: any) => {
+        this.hero = data;
+        console.log(this.hero);
+      }
+    );
   }
 
 }
