486d8639b533fc5726b510c7991d09e36686bd07
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2022-01-10 15:18:05 +0100

    fucking quantity

--- ScorpionBuster2/src/app/pages/game-bar/options-bar/inventory/ng-modal-inventory/ng-modal-inventory.component.ts
+++ ScorpionBuster2/src/app/pages/game-bar/options-bar/inventory/ng-modal-inventory/ng-modal-inventory.component.ts
@@ -1,6 +1,9 @@
+import { compileNgModuleDeclarationExpression } from '@angular/compiler/src/render3/r3_module_compiler';
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { HeroService } from 'src/app/apiServices/hero.service';
 import { ItemsService } from 'src/app/apiServices/items.service';
+import { GameManager } from 'src/app/helpers/gameManager';
 
 @Component({
   selector: 'app-ng-modal-inventory',
@@ -11,9 +14,18 @@ export class NgModalInventoryComponent implements OnInit {
   itemInventory: any;
   description: string = "";
 
-  constructor(public activeModal: NgbActiveModal, private ItemsService: ItemsService) { }
+  constructor(public activeModal: NgbActiveModal, private ItemsService: ItemsService, private HeroService: HeroService) { }
+  heroId = localStorage.getItem("hero") as string;
+  hero: any;
+  sub: any;
+  GM = GameManager.getInstance(this.HeroService, parseInt(this.heroId));
 
   ngOnInit(): void {
+    this.sub = this.GM.Data.subscribe(
+      (data: any) => {
+        this.hero = data;
+      }
+    );
     this.ItemsService.GetInventory().subscribe(
       (data: any) => {
         this.itemInventory = data;
@@ -22,19 +34,48 @@ export class NgModalInventoryComponent implements OnInit {
     );
   }
   choosenItem: number = 0;
-  displayDescription(description: string, id: number){
+  displayDescription(description: string, id: number) {
     this.description = description;
     this.choosenItem = id;
   }
   onUse() {
-    this.itemInventory[this.choosenItem].owned_quantity = this.itemInventory[this.choosenItem].owned_quantity - 1;
-    this.itemInventory[this.choosenItem].owned = false;
-    this.ItemsService.PutItem(this.itemInventory[this.choosenItem]).subscribe(
-            (data: any) => {
-              console.log("OnUse :",data);
+    console.log(this.itemInventory[this.choosenItem].owned_quantity);
+    switch (this.itemInventory[this.choosenItem].itemType) {
+      case "potion": {
+        let temp = this.itemInventory[this.choosenItem];
+        temp.owned_quantity = temp.owned_quantity - 1;
+        if (temp.owned_quantity <= 0) {
+          temp.owned = false;
+        }
+        this.ItemsService.PutItem(temp).subscribe(
+          (data: any) => {
+          }
+        );
+        if(this.hero.hp + temp.statValue > 100) {
+        
+          this.hero.hp = 100;
+        }else{
+          this.hero.hp = this.hero.hp + temp.statValue;
+        }
+        this.GM.dispatch(this.hero);
+        this.ItemsService.GetInventory().subscribe(
+          (data: any) => {
+            this.itemInventory = data;
+          }
+        );
+        break;
+
       }
-    );
+      case "weapon": {
+        break;
+      }
+      case "armor": {
+        break;
+      }
 
+    }
   }
-
-}
+    ngOnDestroy() {
+      this.sub.unsubscribe();
+    }
+  }
