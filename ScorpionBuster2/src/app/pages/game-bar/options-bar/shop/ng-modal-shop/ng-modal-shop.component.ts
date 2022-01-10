4c4e6c186a815e59fea027d7e13753b305e4ca5b
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2022-01-10 13:11:14 +0100

    add singleton on components

--- ScorpionBuster2/src/app/pages/game-bar/options-bar/shop/ng-modal-shop/ng-modal-shop.component.ts
+++ ScorpionBuster2/src/app/pages/game-bar/options-bar/shop/ng-modal-shop/ng-modal-shop.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { HeroService } from 'src/app/apiServices/hero.service';
 import { ItemsService } from 'src/app/apiServices/items.service';
+import { GameManager } from 'src/app/helpers/gameManager';
 
 @Component({
   selector: 'app-ng-modal-shop',
@@ -9,7 +10,9 @@ import { ItemsService } from 'src/app/apiServices/items.service';
   styleUrls: ['./ng-modal-shop.component.css']
 })
 export class NgModalShopComponent implements OnInit {
-  heroId: number = 0;
+  heroId = localStorage.getItem("hero") as string;
+  GM = GameManager.getInstance(this.HeroService, parseInt(this.heroId));
+  sub: any;
   itemShop: any;
   hero: any;
   description: string = "";
@@ -18,24 +21,16 @@ export class NgModalShopComponent implements OnInit {
   constructor(public activeModal: NgbActiveModal, private ItemsService: ItemsService, private HeroService: HeroService) { }
 
   ngOnInit(): void {
-    this.ItemsService.GetItem().subscribe(
-      (data: any) => {
-        this.itemShop = data;
-        console.log(this.itemShop);
-      }
-    );
-
-    //  document.getElementById("btn-item-shop")!.style.backgroundImage = `url('../../../../../../assets/${this.itemShop[this.choosenItem].image}')`;	
-    if (localStorage.getItem("hero") === "Marty") {
-      this.heroId = 5;
-    } else if (localStorage.getItem("hero") === "Bill") {
-      this.heroId = 6;
-    }
-    this.HeroService.GetHeroById(this.heroId).subscribe(
+    this.sub = this.GM.Data.subscribe(
       (data: any) => {
         this.hero = data;
-        console.log(this.hero);
-      });
+      }
+    );
+    this.ItemsService.GetShop().subscribe(
+      (data: any) => {
+        this.itemShop = data;
+      }
+    );
   }
   choosenItem: number = 0;
   displayDescription(description: string, id: number) {
@@ -47,6 +42,7 @@ export class NgModalShopComponent implements OnInit {
     if (this.hero.credit >= this.itemShop[this.choosenItem].price) {
       this.itemShop[this.choosenItem].ownedQuantity = this.itemShop[this.choosenItem].ownedQuantity + 1;
       this.itemShop[this.choosenItem].owned = true;
+      console.log(this.itemShop[this.choosenItem]);
       this.ItemsService.PutItem(this.itemShop[this.choosenItem]).subscribe(
         (data: any) => {
           console.log(data);
--- ScorpionBuster2/src/app/pages/game-bar/statistics-bar/statistics-bar.component.ts
+++ ScorpionBuster2/src/app/pages/game-bar/statistics-bar/statistics-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeroService } from 'src/app/apiServices/hero.service';
+import { GameManager } from 'src/app/helpers/gameManager';
 
 @Component({
   selector: 'app-statistics-bar',
@@ -8,42 +9,38 @@ import { HeroService } from 'src/app/apiServices/hero.service';
   styleUrls: ['./statistics-bar.component.css']
 })
 export class StatisticsBarComponent implements OnInit {
-  heroId: number = 0;
+
+  heroId = localStorage.getItem("hero") as string;
   hero: any;
-  constructor(private HeroService: HeroService, private router:Router) { }
+  sub: any;
+  GM = GameManager.getInstance(this.HeroService, parseInt(this.heroId));
+  constructor(private HeroService: HeroService, private router: Router) { }
   ngOnInit(): void {
-    if (localStorage.getItem("hero") === "Marty") {
-      this.heroId = 5;
-    } else if (localStorage.getItem("hero") === "Bill") {
-      this.heroId = 6;
-    }
-    this.HeroService.GetHeroById(this.heroId).subscribe(
+    let bar = document.getElementById("bar") as HTMLDivElement;
+    this.sub = this.GM.Data.subscribe(
       (data: any) => {
         this.hero = data;
-        console.log(this.hero);
+        bar.style.width = `${this.hero.hp}%`;
       }
     );
+
   }
   onHit() {
     let bar = document.getElementById("bar") as HTMLDivElement;
     let damage = 10;
     if (this.hero.hp - damage >= 1) {
       this.hero.hp = this.hero.hp - damage;
-      this.HeroService.PutHero(this.hero).subscribe(
-        (data: any) => {
-          console.log(data);
-        }
-      );
+      this.GM.dispatch(this.hero);
       bar.style.width = `${this.hero.hp}%`;
     } else if (this.hero.hp - damage < 1) {
       this.hero.hp = 0;
       bar.style.width = `${this.hero.hp}%`;
-      this.HeroService.PutHero(this.hero).subscribe(
-        (data: any) => {
-          console.log(data);
-        }
-      );
+      this.GM.dispatch(this.hero);
       this.router.navigateByUrl('game-over');
     }
   }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
 }
