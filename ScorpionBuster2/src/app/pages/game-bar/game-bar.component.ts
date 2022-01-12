d51251f453d2a199c4b285791c2c03e940ae99e5
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2022-01-12 16:29:32 +0100

    suppr game  bar

--- ScorpionBuster2/src/app/pages/game-bar/game-bar.component.ts
+++ ScorpionBuster2/src/app/pages/game-bar/game-bar.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-bar.component.css']
 })
 export class GameBarComponent implements OnInit {
-
+  hero: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.hero = localStorage.getItem('hero');
   }
 }
