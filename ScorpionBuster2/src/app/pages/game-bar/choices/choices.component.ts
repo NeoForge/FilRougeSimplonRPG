790db6cd6f3e736b001cfee697a5eda4f35cb413
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2022-01-13 10:52:20 +0100

    indices

--- ScorpionBuster2/src/app/pages/game-bar/choices/choices.component.ts
+++ ScorpionBuster2/src/app/pages/game-bar/choices/choices.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from 'src/app/apiServices/hero.service';
+import { IndiceService } from 'src/app/apiServices/indice.service';
 import { GameManager } from 'src/app/helpers/gameManager';
 
 @Component({
@@ -10,20 +11,31 @@ import { GameManager } from 'src/app/helpers/gameManager';
 export class ChoicesComponent implements OnInit {
   localData: any;
   data: any;
+  storyStage = 2;
   GM = GameManager.getInstance(this.HeroService, parseInt(localStorage.getItem("hero") as string));
-  constructor(private HeroService: HeroService) { }
+  indice: any;
+  constructor(private HeroService: HeroService, private IndiceService: IndiceService) { }
 
   ngOnInit(): void {
     this.GM.Data.subscribe(
       (data: any) => {
         this.data = data;
+        this.storyStage = this.data.storyStage;
       }
     );
     this.GM.LocalData.subscribe(
       (data: any) => {
         this.localData = data;
+
       }
     );
+    this.IndiceService.GetIndiceId(this.storyStage).subscribe(
+      (data: any) => {
+        this.indice = data;
+        console.log("tab indices :", this.indice);
+      }
+    );
+
   }
 
   onDialog(nb : number)
